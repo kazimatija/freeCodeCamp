d89c3627321eeb5cc9acacb250eeb075046347fe
kazimatija <mkazimirovic@gmail.com>
2024-01-26 22:59:54 +0100

    Finished step 63

--- Lessons/2_Javascript_Algorithms_and_Data_Structures/Calorie_Counter/script.js
+++ Lessons/2_Javascript_Algorithms_and_Data_Structures/Calorie_Counter/script.js
@@ -7,19 +7,19 @@ const output = document.getElementById('output');
 let isError = false;
 
 function cleanInputString(str) {
-  const regex = /[+-\s]/g;
-  return str.replace(regex, '');
+    const regex = /[+-\s]/g;
+    return str.replace(regex, '');
 }
 
 function isInvalidInput(str) {
-  const regex = /\d+e\d+/i;
-  return str.match(regex);
+    const regex = /\d+e\d+/i;
+    return str.match(regex);
 }
 
 function addEntry() {
-  const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
-  const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length + 1;
-  const HTMLString = `
+    const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
+    const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length + 1;
+    const HTMLString = `
   <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
   <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />
   <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
@@ -29,23 +29,24 @@ function addEntry() {
     id="${entryDropdown.value}-${entryNumber}-calories"
     placeholder="Calories"
   />`;
-  targetInputContainer.insertAdjacentHTML('beforeend', HTMLString);
+    targetInputContainer.insertAdjacentHTML('beforeend', HTMLString);
 }
 
 function getCaloriesFromInputs(list) {
-  let calories = 0;
+    let calories = 0;
 
-  for (let i = 0; i < list.length; i++) {
-    const currVal = cleanInputString(list[i].value);
-    const invalidInputMatch = isInvalidInput(currVal);
+    for (let i = 0; i < list.length; i++) {
+        const currVal = cleanInputString(list[i].value);
+        const invalidInputMatch = isInvalidInput(currVal);
 
-    if (invalidInputMatch) {
-      alert(`Invalid Input: ${invalidInputMatch[0]}`);
-      isError = true;
-      return null;
+        if (invalidInputMatch) {
+            alert(`Invalid Input: ${invalidInputMatch[0]}`);
+            isError = true;
+            return null;
+        }
+        calories += Number(currVal);
     }
 
-  }
 }
 
 addEntryButton.addEventListener("click", addEntry);
