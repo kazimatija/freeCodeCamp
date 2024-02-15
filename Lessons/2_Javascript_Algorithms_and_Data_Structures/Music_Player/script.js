33e00e46a0fca0691d3fa2549ef92a368a190f2a
blueryzen <mkazimirovic@gmail.com>
2024-02-15 17:03:31 +0100

    Finished step 42

--- Lessons/2_Javascript_Algorithms_and_Data_Structures/Music_Player/script.js
+++ Lessons/2_Javascript_Algorithms_and_Data_Structures/Music_Player/script.js
@@ -95,17 +95,27 @@ const playSong = (id) => {
     } else {
         audio.currentTime = userData?.songCurrentTime;
     }
-
     userData.currentSong = song;
+    playButton.classList.add("playing");
 
+    audio.play();
 };
+
+const pauseSong = () => {
+    userData.songCurrentTime = audio.currentTime;
+
+    playButton.classList.remove("playing");
+    audio.pause();
+};
+
+
 
 const renderSongs = (array) => {
     const songsHTML = array
         .map((song) => {
             return `
       <li id="song-${song.id}" class="playlist-song">
-      <button class="playlist-song-info">
+      <button class="playlist-song-info" onclick="playSong(${song.id})">
           <span class="playlist-song-title">${song.title}</span>
           <span class="playlist-song-artist">${song.artist}</span>
           <span class="playlist-song-duration">${song.duration}</span>
@@ -121,6 +131,18 @@ const renderSongs = (array) => {
 
     playlistSongs.innerHTML = songsHTML;
 };
+
+const getCurrentSongIndex = () => userData?.songs.indexOf(userData?.currentSong);
+
+playButton.addEventListener("click", () => {
+    if (userData?.currentSong === null) {
+        playSong(userData?.songs[0].id);
+    } else {
+        playSong(userData?.currentSong.id);
+    }
+});
+
+pauseButton.addEventListener("click", pauseSong);
 
 userData?.songs.sort((a, b) => {
     if (a.title < b.title) {
