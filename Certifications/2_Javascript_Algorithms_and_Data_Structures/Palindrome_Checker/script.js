1b6b428bbc62f2938fe927734e391d7dffaab5cb
kazimatija <mkazimirovic@gmail.com>
2024-02-26 23:30:07 +0100

    Added function to check if input is palindrome

--- Certifications/2_Javascript_Algorithms_and_Data_Structures/Palindrome_Checker/script.js
+++ Certifications/2_Javascript_Algorithms_and_Data_Structures/Palindrome_Checker/script.js
@@ -1,3 +1,17 @@
 const input = document.getElementById("phrase");
 const button = document.getElementById("check");
 const result = document.getElementById("result");
+
+const palindromeChecker = (input) => {
+    const phrase = input.value;
+    const reversedPhrase = phrase
+        .split("")
+        .reverse()
+        .join("");
+
+    if (reversedPhrase === phrase) {
+        result.innerHTML = "The phrase is a palindrome";
+    } else {
+        result.innerHTML = "The phrase is not a palindrome";
+    }
+};
