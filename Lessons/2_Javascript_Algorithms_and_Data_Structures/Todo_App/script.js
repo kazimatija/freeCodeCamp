5900fa2d872a852255e0fe2364dd1c88a9d3b435
blueryzen <mkazimirovic@gmail.com>
2024-03-08 22:28:59 +0100

    Finished step 11

--- Lessons/2_Javascript_Algorithms_and_Data_Structures/Todo_App/script.js
+++ Lessons/2_Javascript_Algorithms_and_Data_Structures/Todo_App/script.js
@@ -23,3 +23,15 @@ closeTaskFormBtn.addEventListener("click", () => {
 
 cancelBtn.addEventListener("click", () => confirmCloseDialog.close());
 
+discardBtn.addEventListener("click", () => {
+    confirmCloseDialog.close();
+    taskForm.classList.toggle("hidden");
+});
+
+
+taskForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
+
+});
