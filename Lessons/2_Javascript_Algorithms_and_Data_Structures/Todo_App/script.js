db6376baabbcf72b6531689d24e7fdbd73bea821
blueryzen <mkazimirovic@gmail.com>
2024-03-10 23:10:51 +0100

    Fixed step 12 mistake

--- Lessons/2_Javascript_Algorithms_and_Data_Structures/Todo_App/script.js
+++ Lessons/2_Javascript_Algorithms_and_Data_Structures/Todo_App/script.js
@@ -33,7 +33,7 @@ taskForm.addEventListener("submit", (e) => {
     e.preventDefault();
 
     const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
-    const titleInputValue = inputField.value.toLowerCase();
-    const id = `${titleInputValue.split(' ').join('-')}`;
-    const taskObj = { id, /* other properties */ };
+    const taskObj = {
+        id: `${titleInput.value.toLowerCase().split(' ').join('-')}`
+    }
 });
