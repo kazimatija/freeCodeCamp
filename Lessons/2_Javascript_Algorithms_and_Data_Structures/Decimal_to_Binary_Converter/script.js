2261714f1b23914c3e7b152135deddae03c28caa
blueryzen <mkazimirovic@gmail.com>
2024-03-26 14:00:52 +0100

    Finished step 3

---
+++ Lessons/2_Javascript_Algorithms_and_Data_Structures/Decimal_to_Binary_Converter/script.js
@@ -0,0 +1,8 @@
+const numberInput = document.getElementById("number-input");
+const convertBtn = document.getElementById("convert-btn");
+const result = document.getElementById("result");
+
+const checkUserInput = () => {
+    console.log(numberInput.value);
+};
+
