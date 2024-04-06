a2fb4f088d9ccd54b9c96dc689f4111ee0f57c5c
blueryzen <mkazimirovic@gmail.com>
2024-04-06 23:23:49 +0200

    Finished step 13

--- Lessons/2_Javascript_Algorithms_and_Data_Structures/Decimal_to_Binary_Converter/script.js
+++ Lessons/2_Javascript_Algorithms_and_Data_Structures/Decimal_to_Binary_Converter/script.js
@@ -1,19 +1,20 @@
-// const numberInput = document.getElementById("number-input");
-// const convertBtn = document.getElementById("convert-btn");
-// const result = document.getElementById("result");
+const numberInput = document.getElementById("number-input");
+const convertBtn = document.getElementById("convert-btn");
+const result = document.getElementById("result");
 
-// const checkUserInput = () => {
-//     if (!numberInput.value || isNaN(parseInt(numberInput.value))) {
+const checkUserInput = () => {
+    if (!numberInput.value || isNaN(parseInt(numberInput.value))) {
+        alert("Please provide a decimal number");
 
-//     }
+    }
 
-//     console.log(numberInput.value);
-// };
+    console.log(numberInput.value);
+};
 
-// convertBtn.addEventListener("click", checkUserInput);
+convertBtn.addEventListener("click", checkUserInput);
 
-// numberInput.addEventListener("keydown", (e) => {
-//     if (e.key === "Enter") {
-//         checkUserInput();
-//     }
-// });
+numberInput.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        checkUserInput();
+    }
+});
