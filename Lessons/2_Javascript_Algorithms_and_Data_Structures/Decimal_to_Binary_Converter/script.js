4078e6e703e29ad3f9dee5c59e82f2de2e939154
blueryzen <mkazimirovic@gmail.com>
2024-04-08 23:08:24 +0200

    Finished step 15

--- Lessons/2_Javascript_Algorithms_and_Data_Structures/Decimal_to_Binary_Converter/script.js
+++ Lessons/2_Javascript_Algorithms_and_Data_Structures/Decimal_to_Binary_Converter/script.js
@@ -2,19 +2,23 @@ const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
-const checkUserInput = () => {
-    if (!numberInput.value || isNaN(parseInt(numberInput.value))) {
-        alert("Please provide a decimal number");
-        return;
-    }
+const decimalToBinary = (input) => {
 
-    console.log(numberInput.value);
+};
+
+const checkUserInput = () => {
+  if (!numberInput.value || isNaN(parseInt(numberInput.value))) {
+    alert("Please provide a decimal number");
+    return;
+  }
+
+  console.log(numberInput.value);
 };
 
 convertBtn.addEventListener("click", checkUserInput);
 
 numberInput.addEventListener("keydown", (e) => {
-    if (e.key === "Enter") {
-        checkUserInput();
-    }
+  if (e.key === "Enter") {
+    checkUserInput();
+  }
 });
