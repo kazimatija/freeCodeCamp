6710f86477aab0f3d14116d67e8c98fbc60ab1b8
blueryzen <mkazimirovic@gmail.com>
2024-04-22 23:59:11 +0200

    test

--- Lessons/2_Javascript_Algorithms_and_Data_Structures/Decimal_to_Binary_Converter/script.js
+++ Lessons/2_Javascript_Algorithms_and_Data_Structures/Decimal_to_Binary_Converter/script.js
@@ -1,37 +1,37 @@
-const numberInput = document.getElementById("number-input");
-const convertBtn = document.getElementById("convert-btn");
-const result = document.getElementById("result");
+// const numberInput = document.getElementById("number-input");
+// const convertBtn = document.getElementById("convert-btn");
+// const result = document.getElementById("result");
 
-const decimalToBinary = (input) => {
-  const inputs = [];
-  const quotients = [];
-  const remainders = [];
+// const decimalToBinary = (input) => {
+//   const inputs = [];
+//   const quotients = [];
+//   const remainders = [];
 
-  while (input > 0) {
-    const quotient = Math.floor(input / 2);
-    const remainder = input % 2;
+//   while (input > 0) {
+//     const quotient = Math.floor(input / 2);
+//     const remainder = input % 2;
 
-    inputs.push(input);
-    quotients.push(quotient);
-    remainders.push(remainder);
-    input = quotient;
-  }
-};
+//     inputs.push(input);
+//     quotients.push(quotient);
+//     remainders.push(remainder);
+//     input = quotient;
+//   }
+// };
 
-const checkUserInput = () => {
-  if (!numberInput.value || isNaN(parseInt(numberInput.value))) {
-    alert("Please provide a decimal number");
-    return;
-  }
+// const checkUserInput = () => {
+//   if (!numberInput.value || isNaN(parseInt(numberInput.value))) {
+//     alert("Please provide a decimal number");
+//     return;
+//   }
 
-  decimalToBinary(parseInt(numberInput.value));
-  numberInput.value = "";
-};
+//   decimalToBinary(parseInt(numberInput.value));
+//   numberInput.value = "";
+// };
 
-convertBtn.addEventListener("click", checkUserInput);
+// convertBtn.addEventListener("click", checkUserInput);
 
-numberInput.addEventListener("keydown", (e) => {
-  if (e.key === "Enter") {
-    checkUserInput();
-  }
-});
+// numberInput.addEventListener("keydown", (e) => {
+//   if (e.key === "Enter") {
+//     checkUserInput();
+//   }
+// });
