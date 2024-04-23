febd3264301a48a3d61c6c59a9fd17f9eecd3369
blueryzen <mkazimirovic@gmail.com>
2024-04-23 23:34:30 +0200

    Finished step 31

--- Lessons/2_Javascript_Algorithms_and_Data_Structures/Decimal_to_Binary_Converter/script.js
+++ Lessons/2_Javascript_Algorithms_and_Data_Structures/Decimal_to_Binary_Converter/script.js
@@ -1,37 +1,39 @@
-// const numberInput = document.getElementById("number-input");
-// const convertBtn = document.getElementById("convert-btn");
-// const result = document.getElementById("result");
+const numberInput = document.getElementById("number-input");
+const convertBtn = document.getElementById("convert-btn");
+const result = document.getElementById("result");
 
-// const decimalToBinary = (input) => {
-//   const inputs = [];
-//   const quotients = [];
-//   const remainders = [];
+const decimalToBinary = (input) => {
+  const inputs = [];
+  const quotients = [];
+  const remainders = [];
 
-//   while (input > 0) {
-//     const quotient = Math.floor(input / 2);
-//     const remainder = input % 2;
+  while (input > 0) {
+    const quotient = Math.floor(input / 2);
+    const remainder = input % 2;
 
-//     inputs.push(input);
-//     quotients.push(quotient);
-//     remainders.push(remainder);
-//     input = quotient;
-//   }
-// };
+    inputs.push(input);
+    quotients.push(quotient);
+    remainders.push(remainder);
+    input = quotient;
+  }
 
-// const checkUserInput = () => {
-//   if (!numberInput.value || isNaN(parseInt(numberInput.value))) {
-//     alert("Please provide a decimal number");
-//     return;
-//   }
+  console.log("Inputs: ", inputs);
+};
 
-//   decimalToBinary(parseInt(numberInput.value));
-//   numberInput.value = "";
-// };
+const checkUserInput = () => {
+  if (!numberInput.value || isNaN(parseInt(numberInput.value))) {
+    alert("Please provide a decimal number");
+    return;
+  }
 
-// convertBtn.addEventListener("click", checkUserInput);
+  decimalToBinary(parseInt(numberInput.value));
+  numberInput.value = "";
+};
 
-// numberInput.addEventListener("keydown", (e) => {
-//   if (e.key === "Enter") {
-//     checkUserInput();
-//   }
-// });
+convertBtn.addEventListener("click", checkUserInput);
+
+numberInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    checkUserInput();
+  }
+});
