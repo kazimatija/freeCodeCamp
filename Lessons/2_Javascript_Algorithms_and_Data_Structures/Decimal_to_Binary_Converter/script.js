16b37639c8050bbf83dfea8713af5685830ce4ea
blueryzen <mkazimirovic@gmail.com>
2024-05-21 22:40:03 +0200

    Finished step 99

--- Lessons/2_Javascript_Algorithms_and_Data_Structures/Decimal_to_Binary_Converter/script.js
+++ Lessons/2_Javascript_Algorithms_and_Data_Structures/Decimal_to_Binary_Converter/script.js
@@ -16,7 +16,8 @@ const animationData = [
   {
     inputVal: 1,
     marginTop: -200,
-    addElDelay: 2000
+    addElDelay: 2000,
+    msg: 'decimalToBinary(1) returns "1" (base case) and gives that value to the stack below. Then it pops off the stack.'
   }
 ];
 
@@ -35,7 +36,7 @@ const showAnimation = () => {
     setTimeout(() => {
       animationContainer.innerHTML += `
         <p id="${obj.inputVal}" style="margin-top: ${obj.marginTop}px;" class="animation-frame">
-
+          decimalToBinary(${obj.inputVal})
         </p>
       `;
     }, obj.addElDelay);
