6e384bed46db5128e43fb0139164d9d99148395a
blueryzen <mkazimirovic@gmail.com>
2024-05-28 23:42:38 +0200

    Finished step 106

--- Lessons/2_Javascript_Algorithms_and_Data_Structures/Decimal_to_Binary_Converter/script.js
+++ Lessons/2_Javascript_Algorithms_and_Data_Structures/Decimal_to_Binary_Converter/script.js
@@ -53,7 +53,9 @@ const showAnimation = () => {
       document.getElementById(obj.inputVal).textContent = obj.msg;
     }, obj.showMsgDelay);
 
+    setTimeout(() => {
 
+    }, obj.removeElDelay);
   });
 };
 
