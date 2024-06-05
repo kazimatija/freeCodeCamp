f6f498eb46ca9a64b3d5d2152b937d820385af8f
blueryzen <mkazimirovic@gmail.com>
2024-06-05 23:20:16 +0200

    Finished step 1

--- Lessons/2_Javascript_Algorithms_and_Data_Structures/Rock_Paper_Scissors/script.js
+++ Lessons/2_Javascript_Algorithms_and_Data_Structures/Rock_Paper_Scissors/script.js
@@ -1,5 +1,12 @@
 function getRandomComputerResult() {
     const options = ["Rock", "Paper", "Scissors"];
-  
-  }
-  console.log(getRandomComputerResult());
+    const randomIndex = Math.floor(Math.random() * options.length);
+    return options[randomIndex];
+}
+
+function hasPlayerWonTheRound(player, computer) {
+
+}
+
+console.log(hasPlayerWonTheRound("Rock", "Scissors"));
+console.log(hasPlayerWonTheRound("Scissors", "Rock")); 
