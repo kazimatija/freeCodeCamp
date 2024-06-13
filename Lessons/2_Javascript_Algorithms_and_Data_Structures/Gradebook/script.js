492747f236ea587d9abc53be3ab2dee13d986b90
blueryzen <mkazimirovic@gmail.com>
2024-06-13 22:24:42 +0200

    Finished step 1

--- Lessons/2_Javascript_Algorithms_and_Data_Structures/Gradebook/script.js
+++ Lessons/2_Javascript_Algorithms_and_Data_Structures/Gradebook/script.js
@@ -1,6 +1,16 @@
 function getAverage(scores) {
-
-}
-
-console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
-console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
+    let sum = 0;
+  
+    for (const score of scores) {
+      sum += score;
+    }
+  
+    return sum / scores.length;
+  }
+  function getGrade(score) {
+  
+  }
+  
+  console.log(getGrade(96));
+  console.log(getGrade(82));
+  console.log(getGrade(56));
