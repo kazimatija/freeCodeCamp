05b81a00a9fed9b50bc8a106d5b12a55208f94f4
blueryzen <mkazimirovic@gmail.com>
2024-08-19 23:57:28 +0200

    Finished step 60

--- Lessons/2_Javascript_Algorithms_and_Data_Structures/Pyramid_Generator/script.js
+++ Lessons/2_Javascript_Algorithms_and_Data_Structures/Pyramid_Generator/script.js
@@ -3,13 +3,13 @@ const count = 8;
 const rows = [];
 
 function padRow(name) {
-  const test = "Testing";  
+  const test = "Testing";
+  console.log("This works!");
   return test;
+  console.log("This works!");
 }
-
 const call = padRow("CamperChan");
 console.log(call);
-
 
 for (let i = 0; i < count; i = i + 1) {
   rows.push(character.repeat(i + 1))
