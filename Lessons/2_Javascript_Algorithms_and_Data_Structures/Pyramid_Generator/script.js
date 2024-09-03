d23410ed7cb5bbc5d627a49586db3bfc40e41262
blueryzen <mkazimirovic@gmail.com>
2024-09-03 23:46:28 +0200

    Finished step 78

--- Lessons/2_Javascript_Algorithms_and_Data_Structures/Pyramid_Generator/script.js
+++ Lessons/2_Javascript_Algorithms_and_Data_Structures/Pyramid_Generator/script.js
@@ -7,9 +7,13 @@ function padRow(rowNumber, rowCount) {
 }
 
 // TODO: use a different type of loop
-/* for (let i = 1; i <= count; i++) {
-   rows.push(padRow(i, count));
-} */
+/*for (let i = 1; i <= count; i++) {
+  rows.push(padRow(i, count));
+}*/
+
+if(true){
+  console.log("Condition is true");
+}
 
 let result = ""
 
