4fa21af2a16074df6df272560a88d4f20bd5e07b
blueryzen <mkazimirovic@gmail.com>
2024-10-03 23:51:31 +0200

    Finished step 10

--- Lessons/2_Javascript_Algorithms_and_Data_Structures/Spam_Filter/script.js
+++ Lessons/2_Javascript_Algorithms_and_Data_Structures/Spam_Filter/script.js
@@ -2,9 +2,9 @@ const messageInput = document.getElementById("message-input");
 const result = document.getElementById("result");
 const checkMessageButton = document.getElementById("check-message-btn");
 
-const helpRegex = /please help/i;
+const helpRegex = /please help|assist me/i;
 
-const isSpam = (msg) => msg.match(helpRegex);
+const isSpam = (msg) => helpRegex.test(msg);
 
 checkMessageButton.addEventListener("click", () => {
   if (messageInput.value === "") {
