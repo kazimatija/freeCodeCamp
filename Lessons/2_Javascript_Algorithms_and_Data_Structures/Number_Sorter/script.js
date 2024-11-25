b5dfa77ae5a02211ae901d15fb190082d14b1e55
blueryzen <mkazimirovic@gmail.com>
2024-11-25 20:37:35 +0100

    Finished step 25

--- Lessons/2_Javascript_Algorithms_and_Data_Structures/Number_Sorter/script.js
+++ Lessons/2_Javascript_Algorithms_and_Data_Structures/Number_Sorter/script.js
@@ -22,7 +22,6 @@ const updateUI = (array = []) => {
 const bubbleSort = (array) => {
   for (let i = 0; i < array.length; i++) {
     for (let j = 0; j < array.length - 1; j++) {
-
       if (array[j] > array[j + 1]) {
         const temp = array[j];
         array[j] = array[j + 1];
@@ -33,5 +32,7 @@ const bubbleSort = (array) => {
 
   return array;
 }
+
+const selectionSort = (array) => { }
 
 sortButton.addEventListener("click", sortInputArray);
