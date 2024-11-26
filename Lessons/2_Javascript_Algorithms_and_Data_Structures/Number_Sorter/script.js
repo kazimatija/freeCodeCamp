fca2ff177eae4f5262dbb3d9c5a792e79c832426
blueryzen <mkazimirovic@gmail.com>
2024-11-26 23:10:09 +0100

    Finished step 26

--- Lessons/2_Javascript_Algorithms_and_Data_Structures/Number_Sorter/script.js
+++ Lessons/2_Javascript_Algorithms_and_Data_Structures/Number_Sorter/script.js
@@ -7,7 +7,7 @@ const sortInputArray = (event) => {
     ...document.getElementsByClassName("values-dropdown")
   ].map((dropdown) => Number(dropdown.value));
 
-  const sortedValues = bubbleSort(inputValues);
+  const sortedValues = selectionSort(inputValues);
 
   updateUI(sortedValues);
 }
@@ -33,6 +33,8 @@ const bubbleSort = (array) => {
   return array;
 }
 
-const selectionSort = (array) => { }
+const selectionSort = (array) => {
+
+}
 
 sortButton.addEventListener("click", sortInputArray);
