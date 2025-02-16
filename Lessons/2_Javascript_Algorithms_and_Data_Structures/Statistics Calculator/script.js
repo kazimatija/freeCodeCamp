996378adcf1e859eb2326125398221ed76a076a6
blueryzen <mkazimirovic@gmail.com>
2025-02-16 17:00:00 +0100

    Finished step 4

--- Lessons/2_Javascript_Algorithms_and_Data_Structures/Statistics Calculator/script.js
+++ Lessons/2_Javascript_Algorithms_and_Data_Structures/Statistics Calculator/script.js
@@ -1,3 +1,5 @@
 const calculate = () => {
-    let value = document.querySelector("#numbers").value;
-}
+    const value = document.querySelector("#numbers").value;
+    const array = value.split(/,\s*/g);
+    const numbers = array.map();
+  }
