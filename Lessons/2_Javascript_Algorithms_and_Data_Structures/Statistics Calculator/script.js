c7041017daa4ff04713be979018e49382dfd27a1
blueryzen <mkazimirovic@gmail.com>
2025-02-18 18:35:58 +0100

    Finished step 25

--- Lessons/2_Javascript_Algorithms_and_Data_Structures/Statistics Calculator/script.js
+++ Lessons/2_Javascript_Algorithms_and_Data_Structures/Statistics Calculator/script.js
@@ -1,9 +1,15 @@
-const getMean = (array) => {
-    const sum = array.reduce((acc, el) => acc + el, 0);
+const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length;
+
+const getMedian = (array) => {
+  const sorted = array.sort((a, b) => a - b);
 }
 
 const calculate = () => {
-    const value = document.querySelector("#numbers").value;
-    const array = value.split(/,\s*/g);
-    const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
+  const value = document.querySelector("#numbers").value;
+  const array = value.split(/,\s*/g);
+  const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
+  
+  const mean = getMean(numbers);
+
+  document.querySelector("#mean").textContent = mean;
 }
